d8b5e60cca7725156af9734fea5fab815412f141
pegakmop <93389368+pegakmop@users.noreply.github.com>
2023-05-30 22:38:16 +0300

    Create desktop.js

---
+++ desktop.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         desktop wiev page
+// @namespace    https://t.me/pegakmop
+// @version      0.1
+// @icon	       https://github.com/pegakmop/pegakmop.github.io/raw/main/PiperPied.png
+// @description  Template userscript created by Stay desktop wiev page
+// @author       @pegakmop
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+(function () {
+    'use strict';
+    // Your code here...
+  var result = [];
+
+document.head.querySelector('meta[name="viewport"]').content = 'width=1024';
+
+completion(result);
+})();
