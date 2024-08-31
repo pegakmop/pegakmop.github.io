0df284eb53bca40e0f747858371ee7bbe7c0500b
pegakmop <93389368+pegakmop@users.noreply.github.com>
2024-09-01 00:16:39 +0300

    Update progressive.js

--- progressive.js
+++ progressive.js
@@ -5,7 +5,7 @@
 
             // Показ баннера для iOS
             if (isIOS && !isStandalone) {
-                const banner = `<div style="position:fixed;bottom:0;width:calc(100% - 2px);background-color:#f8f8f8;border-top:1px solid #ddd;padding:10px;text-align:center;margin:0 1px 1px 1px;">
+                const banner = `<div style="position:fixed;bottom:0;width:calc(100% - 1%);background-color:#f8f8f8;border-top:1px solid #ddd;padding:10px;text-align:center;margin:0 1px 1px 1px;">
                     <span>Установи приложение на iPhone/iPad: </br>Нажми <img src="/ios-share.svg" style="height:20px;vertical-align:middle;" alt="Поделиться">, а затем <img src="/ios-add.svg" style="height:20px;vertical-align:middle;" alt="Добавить"> На экран "Домой"</br></span>
                     <button style="float:center;" onclick="this.parentElement.style.display='none'">Закрыть уведомление</button>
                 </div>`;
@@ -18,7 +18,7 @@
                 window.addEventListener('beforeinstallprompt', (e) => {
                     e.preventDefault();
                     deferredPrompt = e;
-                    const banner = `<div style="position:fixed;bottom:0;width:99%;background-color:#f8f8f8;border-top:1px solid #ddd;padding:10px;text-align:center;">
+                    const banner = `<div style="position:fixed;bottom:0;width:calc(100% - 2px);background-color:#f8f8f8;border-top:1px solid #ddd;padding:10px;text-align:center;">
                         <span>Добавить приложение на домашний экран для быстрого доступа</span>
                         <button onclick="deferredPrompt.prompt();deferredPrompt.userChoice.then(choiceResult => { if (choiceResult.outcome === 'accepted') console.log('User accepted the A2HS prompt'); deferredPrompt = null; });">Установить</button>
                         <button style="float:right;" onclick="this.parentElement.style.display='none'">Закрыть уведомление</button>
@@ -33,7 +33,7 @@
                 window.addEventListener('beforeinstallprompt', (e) => {
                     e.preventDefault();
                     deferredPrompt = e;
-                    const banner = `<div style="position:fixed;bottom:0;width:99%;background-color:#f8f8f8;border-top:1px solid #ddd;padding:10px;text-align:center;">
+                    const banner = `<div style="position:fixed;bottom:0;width:calc(100% - 2px);background-color:#f8f8f8;border-top:1px solid #ddd;padding:10px;text-align:center;">
                         <span>Добавить приложение на домашний экран для быстрого доступа</span>
                         <button onclick="deferredPrompt.prompt();deferredPrompt.userChoice.then(choiceResult => { if (choiceResult.outcome === 'accepted') console.log('User accepted the A2HS prompt'); deferredPrompt = null; });">Установить</button>
                         <button style="float:right;" onclick="this.parentElement.style.display='none'">Закрыть</button>
