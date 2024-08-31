2c9c5740056a58d16f3aa53c145a1b878e87179f
pegakmop <93389368+pegakmop@users.noreply.github.com>
2024-08-31 23:56:30 +0300

    Update progressive.js

--- progressive.js
+++ progressive.js
@@ -7,7 +7,7 @@
             if (isIOS && !isStandalone) {
                 const banner = `<div style="position:fixed;bottom:0;width:99%;background-color:#f8f8f8;border-top:1px solid #ddd;padding:10px;text-align:center;">
                     <span>Установи приложение на iPhone/iPad: </br>Нажми <img src="/ios-share.svg" style="height:20px;vertical-align:middle;" alt="Поделиться">, а затем <img src="/ios-add.svg" style="height:20px;vertical-align:middle;" alt="Добавить"> На экран "Домой"</br></span>
-                    <button style="float:right;" onclick="this.parentElement.style.display='none'">Закрыть уведомление</button>
+                    <button style="float:center;" onclick="this.parentElement.style.display='none'">Закрыть уведомление</button>
                 </div>`;
                 document.body.insertAdjacentHTML('beforeend', banner);
             }
