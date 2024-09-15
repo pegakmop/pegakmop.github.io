5d3a8e3e23239ec9dc2f920c47ed4409cdee09e5
pegakmop <93389368+pegakmop@users.noreply.github.com>
2024-09-15 15:06:07 +0300

    Create DevTools.user.js

---
+++ DevTools.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Auto start DevTools
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Автоматический запуск DevTools на любой странице
+// @author       @pegakmop
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Функция для загрузки и инициализации Eruda
+    function loadEruda() {
+        let script = document.createElement('script');
+        script.src = 'https://cdn.jsdelivr.net/npm/eruda';
+        document.body.appendChild(script);
+        script.onload = function() {
+            eruda.init();
+            eruda.hide();
+        };
+    }
+
+    // Проверяем готовность документа
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', loadEruda);
+    } else {
+        loadEruda();
+    }
+})();
