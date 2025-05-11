3cbf099fe5c18bf625a3fd6c44c42e8dbb73ac1f
pegakmop <93389368+pegakmop@users.noreply.github.com>
2025-05-11 22:06:30 +0300

    Update dns-tls-chatgpt.sh

--- entware/dns-tls-chatgpt.sh
+++ entware/dns-tls-chatgpt.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-# Добавление доменов
+# Добавление доменов chatgpt через днс направляя
 ndmc -c "dns-proxy tls upstream 9.9.9.9 sni dns.quad9.net domain chatgpt.com"
 ndmc -c "dns-proxy tls upstream 9.9.9.9 sni dns.quad9.net domain openai.com"
 ndmc -c "dns-proxy tls upstream 9.9.9.9 sni dns.quad9.net domain oaistatic.com"
 ndmc -c "dns-proxy tls upstream 9.9.9.9 sni dns.quad9.net domain files.oaiusercontent.com"
 ndmc -c "dns-proxy tls upstream 9.9.9.9 sni dns.quad9.net domain gpt3-openai.com"
-ndmc -c "dns-proxy tls upstream 9.9.9.9 sni dns.quad9.net domain openai.fund"
-ndmc -c "dns-proxy tls upstream 9.9.9.9 sni dns.quad9.net domain openai.org"
-# Сохранение конфигурации
+# Сохранение конфигурации установленных днс, чтобы после перезагрузки роутера не сбросились
 ndmc -c "system configuration save"
