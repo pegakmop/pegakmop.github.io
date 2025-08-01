10832ac8fd71249dfa3de36c44e6a8515f26aa5e
pegakmop <93389368+pegakmop@users.noreply.github.com>
2025-08-02 01:56:17 +0300

    Add files via upload

--- entware/proxy.sh
+++ entware/proxy.sh
@@ -1,19 +1,27 @@
-#!/bin/sh
 ndmc -c "components list" | awk '
   $0 ~ /name: proxy/ {found=1}
-  found && /installed: true/ {print; exit}
-' && {
+  found && $1 == "queued:" {
+    if ($2 == "yes") {
+      print "yes"
+      exit
+    } else {
+      print "no"
+      exit
+    }
+  }
+  found && $1 == "component:" { exit }
+' | grep -q "yes" && {
   echo "✅ Компонент клиент прокси установлен"
   echo "⏳ Устанавливаю Proxy0..."
 
   ndmc -c "no interface Proxy0"
   ndmc -c "interface Proxy0"
-
-  ndmc -c "interface Proxy0 description mihomo-Proxy0-$(ip -4 -o addr show br0 2>/dev/null | awk '"'"'{print $4}'"'"' | cut -d/ -f1 | head -n1):1080"
+  
+  ndmc -c "interface Proxy0 description mihomo-Proxy0-$(ip -4 -o addr show br0 2>/dev/null | awk '{print $4}' | cut -d/ -f1 | head -n1):1080"
   ndmc -c "interface Proxy0 proxy protocol socks5"
   ndmc -c "interface Proxy0 proxy socks5-udp"
   ndmc -c "interface Proxy0 proxy upstream $(ip -4 -o addr show br0 2>/dev/null | awk '{print $4}' | cut -d/ -f1 | head -n1) 1080"
-
+  
   ndmc -c "interface Proxy0 up"
   ndmc -c "interface Proxy0 ip global 1"
   ndmc -c "system configuration save"
@@ -23,6 +31,7 @@ ndmc -c "components list" | awk '
   echo "❌ Компонент КЛИЕНТ ПРОКСИ не установлен!"
   echo "➡️ В веб-интерфейсе Keenetic: http://$(ip -4 -o addr show br0 2>/dev/null | awk '{print $4}' | cut -d/ -f1 | head -n1)"
   echo "В боковом меню ищем → Параметры системы → Изменить набор компонентов → Клиент прокси → включите галочку и сохраните."
-  echo "❗️Роутер перезагрузится после сохранения. Потом запустите данный скрипт заново."
+  echo "❗️Роутер перезагрузится после сохранения, для установки компонента клиент прокси."
+  echo "⚠️ Потом запустите данный скрипт заново"
   exit 1
 }
