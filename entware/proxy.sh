87b5d0510d37d44e289c1a667e2f2bb9b82d671b
pegakmop <93389368+pegakmop@users.noreply.github.com>
2025-08-04 02:18:17 +0300

    Update proxy.sh

--- entware/proxy.sh
+++ entware/proxy.sh
@@ -13,20 +13,17 @@ ndmc -c "components list" | awk '
 ' | grep -q "yes" && {
   echo "✅ Компонент клиент прокси установлен"
   echo "⏳ Устанавливаю Proxy0..."
-
   ndmc -c "no interface Proxy0"
   ndmc -c "interface Proxy0"
-  
   ndmc -c "interface Proxy0 description pegakmop-Proxy0-$(ip -4 -o addr show br0 2>/dev/null | awk '{print $4}' | cut -d/ -f1 | head -n1):1080"
   ndmc -c "interface Proxy0 proxy protocol socks5"
   ndmc -c "interface Proxy0 proxy socks5-udp"
   ndmc -c "interface Proxy0 proxy upstream $(ip -4 -o addr show br0 2>/dev/null | awk '{print $4}' | cut -d/ -f1 | head -n1) 1080"
-  
   ndmc -c "interface Proxy0 up"
   ndmc -c "interface Proxy0 ip global 1"
   ndmc -c "system configuration save"
-
   echo "✅ Proxy0 успешно создан и сохранён!"
+  exit 1
 } || {
   echo "❌ Компонент КЛИЕНТ ПРОКСИ не установлен!"
   echo "➡️ В веб-интерфейсе: http://$(ip -4 -o addr show br0 2>/dev/null | awk '{print $4}' | cut -d/ -f1 | head -n1)"
