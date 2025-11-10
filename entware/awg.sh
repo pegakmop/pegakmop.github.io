48756ec2ddd1d1313d42c6b5950b4f089868d0d7
pegakmop <93389368+pegakmop@users.noreply.github.com>
2025-11-11 01:52:58 +0300

    Update messages and IP address handling in awg.sh

--- entware/awg.sh
+++ entware/awg.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "Приветствую всех запустивших и просматривающих скрипт, @pegakmop снова с вами, по всем вопросам пишите мне в телеграмм"
 echo ""
-echo "Данный скрипт представляет собой генерацию WARP конфигурации Cloudflare  и установку её на роутеры фирмы Keenetic при активной поддержке двух сайтов, если бы не они я бы и не стал заморачиваться с данным скриптом"
-logger "Данный скрипт представляет собой генерацию WARP конфигурации Cloudflare  и установку её на роутеры фирмы Keenetic при активной поддержке двух сайтов, если бы не они я бы и не стал заморачиваться с данным скриптом"
+echo "Данный скрипт представляет собой генерацию WARP конфигурации Cloudflare  и установку её на роутеры фирмы Keenetic при активной поддержке сайта, если бы не они я бы и не стал заморачиваться с данным скриптом"
+logger "Данный скрипт представляет собой генерацию WARP конфигурации Cloudflare  и установку её на роутеры фирмы Keenetic при активной поддержке сайта, если бы не они я бы и не стал заморачиваться с данным скриптом"
 echo ""
 
 if ! opkg list-installed | grep -q '^jq '; then
@@ -11,7 +11,7 @@ if ! opkg list-installed | grep -q '^jq '; then
     opkg update
     opkg install jq
 else
-    echo "jq уже установлен, это радует"
+    echo ""
 fi
 echo ""
 interfaces=$(ls /sys/class/net/ | grep '^nwg[0-9]\+$')
@@ -22,17 +22,18 @@ else
     iface_num=$((max + 1))
 fi
 iface_name="Wireguard$iface_num"
+address_ip4_custom="172.16.$iface_num.2"
 configure_wireguard() {
   ndmc -c "no interface $iface_name" >/dev/null 2>&1
   ndmc -c "system configuration save" >/dev/null 2>&1
   echo "Устанавливаю конфигурацию на роутер..."
   logger "Устанавливаю конфигурацию на роутер..."
-  logger "Возможно будет 1 или 2 ошибки красным гореть, не обращайте внимания на них, если у вас интернет только ip4, то это ошибки попытки добавления ip6 адресов. Если вы пользуетесь ip4 + ip6 ошибок не будет вообще."
+  logger "Возможно будет 1 или 2 ошибки красным гореть, не обращайте внимания на них, если у вас интернет только ip4, то это ошибки попытки добавления ip6 адресов. Если вы пользуетесь ip4 + ip6 ошибок добавления не будет вообще."
   echo ""
   sleep 3
   ndmc -c "interface $iface_name" >/dev/null 2>&1
   ndmc -c "interface $iface_name description @pegakmop-$iface_name" >/dev/null 2>&1
-  ndmc -c "interface $iface_name ip address $address_ip4/32" >/dev/null 2>&1
+  ndmc -c "interface $iface_name ip address $address_ip4_custom/32" >/dev/null 2>&1
   ndmc -c "interface $iface_name ip address $address_ip6/128" >/dev/null 2>&1
   ndmc -c "interface $iface_name wireguard private-key $private_key" >/dev/null 2>&1
   ndmc -c "interface $iface_name wireguard listen-port $endpoint_port" >/dev/null 2>&1
@@ -55,37 +56,23 @@ configure_wireguard() {
   echo "Установка конфигурации на роутер завершена."
   logger "Установка конфигурации на роутер завершена."
   echo ""
-  echo "P.S. если генерируете более одного конфига и он не подключается, нажми на конфиг и смени ip 4 адрес с условного 172.16.0.2/32 на условно 172.16.0.3/32 или любой другой который тебе удобен и сохрани конфиг, не допустимо пересечение подсети между конфигурациями"
-  logger "P.S. если генерируете более одного конфига и он не подключается, нажми на конфиг и смени ip 4 адрес с условного 172.16.0.2/32 на условно 172.16.0.3/32 или любой другой который тебе удобен и сохрани конфиг, не допустимо пересечение подсети между конфигурациями"
+  echo "P.S. если генерируете более одного конфига и один из них не подключается, нажми на конфиг и после удали конфиг"
+  logger "P.S. если генерируете более одного конфига и один из них не подключается, нажми на конфиг и после удали конфиг"
 }
 
+#echo "Используется источник: warp-gen.vercel.app"
+response=$(curl -s https://warp-gen.vercel.app/generate-config)
+success=$(echo "$response" | jq -r '.success')
 
-while true; do
-  echo "Выберите источник генерации конфига:"
-  echo "1 - warp-gen.vercel.app"
-  read -r -p "Ваш выбор ответ цифрой (1): " choice
+if [ "$success" != "true" ]; then
+    echo "Ошибка генерации конфига, попробуйте еще раз, либо вернитесь позднее, заодно напиши @pegakmop пусть проверит всё ли нормально."
+    logger "Ошибка генерации конфига, попробуйте еще раз, либо вернитесь позднее, заодно напиши @pegakmop пусть проверит всё ли нормально."
+    exit 1
+fi
 
-  case "$choice" in
-    1)
-      echo "Вы выбрали источник: warp-gen.vercel.app"
-      response=$(curl -s https://warp-gen.vercel.app/generate-config)
-      success=$(echo "$response" | jq -r '.success')
+config=$(echo "$response" | jq -r '.config')
+cleaned_config=$(echo "$config" | grep -v -e '^\[Interface\]$' -e '^\[Peer\]$' -e '^$')
 
-      if [ "$success" != "true" ]; then
-        echo "Ошибка генерации конфига, попробуйте еще раз, либо вернитесь позднее, заодно напиши @pegakmop пусть проверит всё ли нормально."
-        logger "Ошибка генерации конфига, попробуйте еще раз, либо вернитесь позднее, заодно напиши @pegakmop пусть проверит всё ли нормально."
-        exit 1
-      fi
-
-      config=$(echo "$response" | jq -r '.config')
-      cleaned_config=$(echo "$config" | grep -v -e '^\[Interface\]$' -e '^\[Peer\]$' -e '^$')
-      break
-      ;;
-    *)
-      echo "Неверный выбор. Пожалуйста, введите 1."
-      ;;
-  esac
-done
 echo ""
 echo "Генерирую конфигурацию..."
 logger "Генерирую конфигурацию..."
