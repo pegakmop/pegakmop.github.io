ff08c8b2feaffa86995b2292af2d656847dd2f82
pegakmop <93389368+pegakmop@users.noreply.github.com>
2026-02-15 01:42:30 +0300

    Update logging to use script name in 020-3x-ui.sh

--- entware/020-3x-ui.sh
+++ entware/020-3x-ui.sh
@@ -1,85 +1,86 @@
 #!/opt/bin/sh
 # chmod +x /opt/etc/ndm/netfilter.d/020-3x-ui.sh
 # /opt/etc/ndm/netfilter.d/020-3x-ui.sh
+
+# Получаем имя скрипта автоматически
+SCRIPT_NAME=$(basename "$0")
+
+# Проверка и установка iptables при первом запуске
+if [ ! -f /opt/sbin/iptables ]; then
+    logger "$SCRIPT_NAME: iptables not found, installing..."
+    opkg update
+    opkg install iptables
+    if [ $? -eq 0 ]; then
+        logger "$SCRIPT_NAME: iptables successfully installed"
+    else
+        logger "$SCRIPT_NAME: ERROR - failed to install iptables"
+        exit 1
+    fi
+fi
+
 # Ждём 5 секунд, чтобы интерфейсы tun+, t2s+ и opkgtun+ успели появиться
 sleep 5
 
 # Функция для проверки существования правила
 rule_exists() {
-    iptables-save | grep -q -- "$1"
+    /opt/sbin/iptables-save | grep -q -- "$1"
 }
 
 # Добавляем правила для tun+
 RULE="-A INPUT -i tun+ -j ACCEPT"
 if ! rule_exists "$RULE"; then
     /opt/sbin/iptables -A INPUT -i tun+ -j ACCEPT
-    logger "020-x-ui.sh: Added rule: $RULE"
-else
-    :
+    logger "$SCRIPT_NAME: Added rule: $RULE"
 fi
 
 RULE="-A FORWARD -i tun+ -j ACCEPT"
 if ! rule_exists "$RULE"; then
     /opt/sbin/iptables -A FORWARD -i tun+ -j ACCEPT
-    logger "020-x-ui.sh: Added rule: $RULE"
-else
-    :
+    logger "$SCRIPT_NAME: Added rule: $RULE"
 fi
 
 RULE="-A FORWARD -o tun+ -j ACCEPT"
 if ! rule_exists "$RULE"; then
     /opt/sbin/iptables -A FORWARD -o tun+ -j ACCEPT
-    logger "020-x-ui.sh: Added rule: $RULE"
-else
-    :
+    logger "$SCRIPT_NAME: Added rule: $RULE"
 fi
 
 # Добавляем правила для t2s+
 RULE="-A INPUT -i t2s+ -j ACCEPT"
 if ! rule_exists "$RULE"; then
     /opt/sbin/iptables -A INPUT -i t2s+ -j ACCEPT
-    logger "020-x-ui.sh: Added rule: $RULE"
-else
-    :
+    logger "$SCRIPT_NAME: Added rule: $RULE"
 fi
 
 RULE="-A FORWARD -i t2s+ -j ACCEPT"
 if ! rule_exists "$RULE"; then
     /opt/sbin/iptables -A FORWARD -i t2s+ -j ACCEPT
-    logger "020-x-ui.sh: Added rule: $RULE"
-else
-    :
+    logger "$SCRIPT_NAME: Added rule: $RULE"
 fi
 
 RULE="-A FORWARD -o t2s+ -j ACCEPT"
 if ! rule_exists "$RULE"; then
     /opt/sbin/iptables -A FORWARD -o t2s+ -j ACCEPT
-    logger "020-x-ui.sh: Added rule: $RULE"
-else
-    :
+    logger "$SCRIPT_NAME: Added rule: $RULE"
 fi
 
 # Добавляем правила для opkgtun+
 RULE="-A INPUT -i opkgtun+ -j ACCEPT"
 if ! rule_exists "$RULE"; then
     /opt/sbin/iptables -A INPUT -i opkgtun+ -j ACCEPT
-    logger "020-x-ui.sh: Added rule: $RULE"
-else
-    :
+    logger "$SCRIPT_NAME: Added rule: $RULE"
 fi
 
 RULE="-A FORWARD -i opkgtun+ -j ACCEPT"
 if ! rule_exists "$RULE"; then
     /opt/sbin/iptables -A FORWARD -i opkgtun+ -j ACCEPT
-    logger "020-x-ui.sh: Added rule: $RULE"
-else
-    :
+    logger "$SCRIPT_NAME: Added rule: $RULE"
 fi
 
 RULE="-A FORWARD -o opkgtun+ -j ACCEPT"
 if ! rule_exists "$RULE"; then
     /opt/sbin/iptables -A FORWARD -o opkgtun+ -j ACCEPT
-    logger "020-x-ui.sh: Added rule: $RULE"
-else
-    :
+    logger "$SCRIPT_NAME: Added rule: $RULE"
 fi
+
+logger "$SCRIPT_NAME: Script completed successfully"
