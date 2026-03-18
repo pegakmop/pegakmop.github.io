6cd71a7d577ac5e6a8386b823bfe610c275bcba4
pegakmop <93389368+pegakmop@users.noreply.github.com>
2026-03-18 15:17:13 +0300

    Create dns-dot-claude.sh

---
+++ entware/dns-dot-claude.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "Добавление доменов chatgpt через днс направляя"
+ndmc -c "dns-proxy tls upstream 9.9.9.9 sni dns.quad9.net domain claude.ai"
+ndmc -c "dns-proxy tls upstream 9.9.9.9 sni dns.quad9.net domain claude.app"
+ndmc -c "dns-proxy tls upstream 9.9.9.9 sni dns.quad9.net domain anthropic.com"
+ndmc -c "dns-proxy tls upstream 9.9.9.9 sni dns.quad9.net domain statsig.com"
+ndmc -c "dns-proxy tls upstream 9.9.9.9 sni dns.quad9.net domain statsigapi.net"
+echo "Сохранение конфигурации установленных днс, чтобы после перезагрузки роутера не сбросились"
+ndmc -c "system configuration save"
